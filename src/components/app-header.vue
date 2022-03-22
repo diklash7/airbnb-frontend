91ae81db56064c39a9768e7271792cbd08f62262
itsar <itsar302@gmail.con>
2022-03-22 20:27:17 +0200

    first

---
+++ src/components/app-header.vue
@@ -0,0 +1,12 @@
+
+<template>
+   <h1>LOGO Airbnb</h1>
+   
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+
+ </template>
+
+
