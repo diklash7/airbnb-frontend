655c3215ae1cbbac159b85b05277dfe28c2a0236
Elana Hecht <elanabrick@gmail.com>
2022-03-23 15:29:40 +0200

    commit better homepage

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import store from './store'
 // Styles
 import './styles/style.scss'
 
+
 const app = createApp(App)
+
+app.config.globalProperties.$filters = {
+   currencyUSD(amount) {
+     // look implementation inside car-preview.vue
+     return '$' + amount
+   },
+ }
 
 app.use(router)
 app.use(store)
