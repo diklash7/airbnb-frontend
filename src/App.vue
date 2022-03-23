051f48092d4faa78af0ee30d2f29d02847c86c7c
dikla cohen <diklash7@gmail.com>
2022-03-23 17:31:57 +0200

    adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <app-header/>
   <router-view/>
+  <app-footer/>
 </template>
 
 <script>
 import appHeader from "./components/app-header.vue";
+import appFooter from "./components/app-footer.vue";
 
 export default {
   name: "app",
   components: {
     appHeader,
+    appFooter
   },
 };
 </script>
---
+++ src/components/app-footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <footer class="app-footer-container full main-root">
+    <div class="app-footer">
+      <div class="app-footer-column">
+        <h3>Support</h3>
+        <h4>Help Center</h4>
+        <h4>Safety Information</h4>
+        <h4>Cancellation options</h4>
+        <h4>Our COVID-19 Response</h4>
+        <h4>Supporting people with disabilities</h4>
+        <h4>Report a neghborhood concern</h4>
+      </div>
+      <div class="app-footer-column">
+        <h3>Community</h3>
+        <h4>Support Afghan refugees</h4>
+        <h4>Celebrating diversity & belonging</h4>
+        <h4>Combating discrimination</h4>
+      </div>
+
+      <div class="app-footer-column">
+        <h3>Try hosting</h3>
+        <h4>AirCover: protection for Hosts</h4>
+        <h4>Explore hosting resources</h4>
+        <h4>Visit our community forum</h4>
+        <h4>How to host responsibly</h4>
+      </div>
+      <div class="app-footer-column">
+        <h3>About</h3>
+        <h4>Newsroom</h4>
+        <h4>Learn about new features</h4>
+        <h4>Letter from our founders</h4>
+        <h4>Careers</h4>
+        <h4>Investors</h4>
+        <h4>Airbnb Luxe</h4>
+      </div>
+    </div>
+    <!-- <p>&copy; CaJan22</p> -->
+  </footer>
+</template>
+
+<style>
+.app-footer-column {
+  padding: 1.2rem 1.2rem 1.2rem 0;
+}
+
+.app-footer-container {
+  align-items: center;
+  background-color: #f8f8f8;
+  color: #464646;
+  display: flex;
+  flex-direction: column;
+  font-weight: 400;
+  margin-top: 4rem;
+  width: 100%;
+}
+</style>
+
+
+
+
