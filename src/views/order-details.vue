fdfa7b0e68a239dc9d88bae5bc4ea40472cfdd0c
Elana Hecht <elanabrick@gmail.com>
2022-04-01 09:59:32 +0300

    friday morning

--- src/views/order-details.vue
+++ src/views/order-details.vue
@@ -4,20 +4,35 @@
     <section class="order-details-container flex space-between">
       <div class="order-date-guest">
         <div class="order-title flex">
-          <button @click="$router.back()">{{ "◀" }}</button>
-          <h1>Your trip</h1>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="green"
+              class="bi bi-check-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"
+              />
+            </svg>
+          <h1>Reserved successfully!</h1>
         </div>
-          <div class="rare-stay">
-            <h4>This is a rare find.</h4>
-            <p>{{ stay.host.fullname }}'s place is usually booked</p>
-          </div>
+        <div class="rare-stay">
+          <h4>This is a rare find.</h4>
+          <p>{{ stay.host.fullname }}'s place is usually booked</p>
+        </div>
         <div class="dates-order-container flex flex-column space-between">
-        <div class="dates-order">
-          <div>
-            <h3>Dates:</h3>
-            <span>May 1 – 4</span>
-          </div>
-          <router-link class="btn-edit" to="/">Edit</router-link>
+          <p>Your trip</p>
+          <div class="dates-order">
+            <div>
+              <h3>Dates:</h3>
+              <span>May 1 – 4</span>
+            </div>
+            <router-link class="btn-edit" to="/">Edit</router-link>
           </div>
           <div class="guests-order">
             <div>
@@ -40,6 +55,7 @@
             </div>
           </div>
         </div>
+          <h2>Price Details</h2>
         <div class="type-price">
           <div class="type-pay">
             {{ $filters.currencyUSD(this.stay.price) }} x 3 nights
@@ -84,7 +100,7 @@ export default {
       return this.stay.price * 3;
     },
     totalPay() {
-      return this.stay.price * 3 + 150 + 200;
+      return new Intl.NumberFormat('en-US').format((this.stay.price * 3 + 150 + 200));
     },
   },
   components: {
